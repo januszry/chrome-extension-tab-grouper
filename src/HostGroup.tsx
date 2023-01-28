da8ee2751852a87bc087a0f1d28f99f310b8d050
Zeng Ruoyu <ruoyu.zeng@shopee.com>
2023-01-28 11:44:54 +0800

    Split buttons

--- src/HostGroup.tsx
+++ src/HostGroup.tsx
@@ -8,15 +8,36 @@ const GroupTitle = styled.h1`
   text-overflow: ellipsis;
   margin: 0px;
   flex-shrink: 0;
+  flex-grow: 1;
 `;
 
-const GroupToCurrentWindow = styled.button`
+const CreateGroupButton = styled.button`
+  border-width: 0;
+  border-radius: 2px;
+  background-color: green;
+  color: white;
+  width: 80px;
+`;
+
+const MoveGroupHereButton = styled.button`
+  border-width: 0;
+  border-radius: 2px;
+  background-color: grey;
+  color: white;
+  width: 80px;
+`;
+
+const DismissButton = styled.button`
+  border-width: 0;
+  border-radius: 2px;
+  background-color: red;
+  color: white;
+  width: 80px;
 `;
 
 const GroupTitleFlexContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   margin: 10px;
 `;
 
@@ -26,9 +47,6 @@ const GroupContent = styled.ul`
 `;
 
 const GroupContainer = styled.div`
-  &:not(:first-child) {
-    margin-top: 20px;
-  }
 `;
 
 interface HostGroupProps {
@@ -42,40 +60,50 @@ interface HostGroupProps {
 class HostGroup extends React.Component<HostGroupProps, {}> {
   constructor(props: HostGroupProps | Readonly<HostGroupProps>) {
     super(props);
-    this.handleClickGroupAction = this.handleClickGroupAction.bind(this);
+    this.handleCreateGroup = this.handleCreateGroup.bind(this);
+    this.handleMoveGroupHere = this.handleMoveGroupHere.bind(this);
+    this.handleDismissGroup = this.handleDismissGroup.bind(this);
   }
 
-  async handleClickGroupAction() {
-    const { group, activeTab, host, tabs } = this.props;
+  fullyGroupedInCurrentWindow(): boolean | undefined {
+    const { group, activeTab } = this.props;
+    return group && activeTab && group.windowId == activeTab.windowId;
+  }
+
+  async handleCreateGroup() {
+    const { activeTab, host, tabs } = this.props;
     const tabIds = tabs.map(tab => tab.id!);
     if (tabIds.length === 0) {
       return;
     }
-    if (group) {
-      // If all tabs belong to a same group, only move the group to current window if not
-      if (activeTab && group.windowId != activeTab.windowId) {
-        await chrome.tabGroups.move(group.id, { windowId: activeTab.windowId, index: -1 });
-        await chrome.tabs.update(tabIds[0], { active: true });
-      }
-      return;
-    }
     const newGroup = await chrome.tabs.group({ tabIds });
     await chrome.tabGroups.update(newGroup, { title: host! });
-    if (activeTab && activeTab.id && !tabIds.includes(activeTab.id)) {
-      await chrome.tabs.update(tabIds[0], { active: true });
-    }
+  }
+
+  async handleMoveGroupHere() {
+    const { group, activeTab } = this.props;
+    await chrome.tabGroups.move(group!.id, { windowId: activeTab!.windowId, index: -1 });
+  }
+
+  async handleDismissGroup() {
+    return await chrome.tabs.ungroup(this.props.tabs.map(tab => tab.id!));
   }
 
   render() {
-    const { group, groups, activeTab, host, tabs } = this.props;
+    const { group, groups, host, tabs } = this.props;
     const listItems = tabs.map((tab: chrome.tabs.Tab) => <TabItem tab={tab} group={groups.get(tab.groupId)} />)
-    const toHideButton = group && activeTab && group.windowId == activeTab.windowId;
     return (
       <GroupContainer>
         <GroupTitleFlexContainer>
           <GroupTitle>{host}</GroupTitle>
           {
-            toHideButton ? undefined : <GroupToCurrentWindow onClick={this.handleClickGroupAction}>{!group ? "Group" : "Move here"}</GroupToCurrentWindow>
+            group == undefined
+            ? <CreateGroupButton onClick={this.handleCreateGroup}>Create</CreateGroupButton>
+            : (
+              this.fullyGroupedInCurrentWindow()
+              ? <DismissButton onClick={this.handleDismissGroup}>Dismiss</DismissButton>
+              : <MoveGroupHereButton onClick={this.handleMoveGroupHere}>To Here</MoveGroupHereButton>
+            )
           }
         </GroupTitleFlexContainer>
         <GroupContent>{listItems}</GroupContent>
